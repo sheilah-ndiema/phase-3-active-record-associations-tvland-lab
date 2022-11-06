b8ceadbf4b3a087087bd4e6221fca00ebeec926a
Ndiema-S <you@example.com>
2022-11-06 16:54:35 +0300

    complete assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map {|a| actor.full_name}
+  end
+
+
 end
